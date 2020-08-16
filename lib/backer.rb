ecdd85d8f100c32bc522eea83f4ea78b01527c72
Nathan Steed <steedn@outlook.com>
2020-08-16 01:32:36 +0000

    Automatically backed up by Learn

---
+++ lib/backer.rb
@@ -0,0 +1,11 @@
+class Backer
+  attr_reader :backed_projects
+  attr_accessor :name
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+  end
+  def back_project(project)
+    @backed_projects << project
+  end
+end
---
+++ lib/project.rb
@@ -0,0 +1,13 @@
+require "pry"
+
+class Project
+attr_accessor :title, :backers
+def initialize(title)
+  @title = title
+  @backers = []
+  end
+  def add_backer(name)
+    @backers << name
+    # binding.pry
+  end
+end
